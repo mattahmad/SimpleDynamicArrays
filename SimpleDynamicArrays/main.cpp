423746d59fdbf846b05a3983d01494e46bd29ad7
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-25 18:10:06 -0400

    Initial Commit

---
+++ SimpleDynamicArrays/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleDynamicArrays
+//
+//  Created by Matt Ahmad on 2022-10-25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
